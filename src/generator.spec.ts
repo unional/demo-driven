b507d24075948cbe4ad8bd7338c4a96c7f4d35c4
Homa Wong <homawong@gmail.com>
2017-03-18 01:10:49 -0700

    Fix tests

--- src/generator.spec.ts
+++ src/generator.spec.ts
@@ -3,14 +3,14 @@ import test from 'ava'
 import { generate, parse } from './generator'
 
 test('empty content produces empty result', async t => {
-  t.is(await generate(''), '\n')
+  t.is(await generate(''), '')
 })
 
 test('without metadata', async t => {
   t.is(await generate(`# First Header`), '<h1>First Header</h1>')
 })
 
-test.only('with metadata', async t => {
+test('with metadata', async t => {
   t.is(await generate(`---
 title: demo
 ---
